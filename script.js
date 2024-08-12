63cb0152955f009930bc2840d592416a3b0088f6
Rachel Burkhardt <r.burkhardt.ibclc@gamil.com>
2024-08-12 15:09:51 -0500

    starting over. following the steps again for task 2

--- script.js
+++ script.js
@@ -2,16 +2,21 @@
 
 window.addEventListener("load", function() {
 
-    let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
-    listedPlanetsResponse.then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    })
+    // let listedPlanets;
+    // // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    // let listedPlanetsResponse;
+    // listedPlanetsResponse.then(function (result) {
+    //     listedPlanets = result;
+    //     console.log(listedPlanets);
+    // }).then(function () {
+    //     console.log(listedPlanets);
+    //     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    // })
+
+    formSubmit.addEventListener("click", function(event){
+        event.preventDefault();
+        formSubmission(form, faultyItems, pilotName.value, copilotName.value, fuelLevel.value, cargoMass.value);
+    });
 
    
     
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,6 +1,5 @@
 // Write your helper functions here!
 
-require('cross-fetch/polyfill');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     // Here is the HTML formatting for our mission target div.
@@ -23,7 +22,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
     const copilotName = document.getElementsByName("copilotName");
     const fuelLevel = document.getElementsByName("fuelLevel");
     const cargoMass = document.getElementsByName("cargoMass");
-    const form = document.querySelector("form");
+    const form = document.getElementsByName("testForm");
 
     //TODO: set up object variables for the list items
     const pilotStatus = document.getElementById("pilotStatus");
@@ -35,119 +34,91 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
     //TODO: set up form and buttons
     const launchForm = document.getElementById("launchForm");
     const formSubmit = document.getElementById("formSubmit");
+    const faultyItems = document.getElementById("FaultyItems");
+    let testInput = [pilotName, copilotName, fuelLevel, cargoMass];
+
+
    
 
  function validateInput(testInput) {
 
-    
-
-
 
     //TODO: setup validation for inputs
-    if (pilotName === ""){
+    if (pilotName.value === ""){
         window.alert("You must enter the Pilot's name.");
-        return "Empty";
-        
+        return "Empty";        
     } 
     
-    if (copilotName === "") {
+    if (copilotName.value === "") {
         window.alert("You must enter the copilot's name.");
         return "Empty";
     } 
     
-    if (isNaN(fuelLevel)) {
+    if (isNaN(fuelLevel.value)) {
         window.alert("You must enter a valid number for the fuel level.");
         return "Not a Number";
-    } else if (!isNaN(fuelLevel)){
+    } else if (!isNaN(fuelLevel.value)){
         return "Is a Number";
     }
 
-    if (isNaN(cargoMass)) {
+    if (isNaN(cargoMass.value)) {
         window.alert("You must enter a valid number for the cargo mass.")
         return "Not a Number";
-    } else if (!isNaN(cargoMass)) {
+    } else if (!isNaN(cargoMass.value)) {
         return "Is a Number";
+    } else {
+        formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
     }
+    
 
  };
 
 
- //------------------------------------------------------------------------------------------------------------------------------------------------
- //NOTE: pilot ".getElementById("pilotName")" and copilot ".getElementsByName("copilotName")" values = "strings"     NOTE: will return as "" (empty) if no input
- //NOTE: fuel level ")" and cargo mass ".getElementsByName("cargoMass")" values = "numbers"    NOTE: will return NaN or is a number 
-
- //NOTE: If you want to check if something is NaN, you cannot use == or ===. Instead, JavaScript has a built-in method called isNaN(value) that returns true if value is NaN and false if value is not NaN.
-
- //NOTE: the validateInput() will complete the formSubmission() 
-
- //NOTE: formSubmission() will take in a document parameter and strings representing the pilot, co-pilot, fuel level and cargo mass. 
-
- //NOTE: using the values in those strings and the document parameter for your HTML document, update the shuttle requirements as described below:
-
-        //TODO: the <div> "faultyItems" should update if something is NOT ready for launch
-
-        //TODO: Use template literals, update the <li> elements "pilotStatus" and "copilotStatus" to include the names of pilot and copilot. 
-
-        //TODO: If the user submits a fuel level that is too low (less than 10,000 liters), change faultyItems to visible with an updated fuel status stating that there is not enough fuel for the journey.
-
-        //TODO: The text of the h2 element, launchStatus, should also change to “Shuttle not ready for launch” and the color should change to red.
-
-        //TODO: If the user submits a cargo mass that is too large (more than 10,000 kilograms), change the list to visible with an updated cargo status stating that there is too much mass for the shuttle to take off.
-
-        //TODO: The text of launchStatus should also change to “Shuttle not ready for launch” and the color should change to red.
-
-        //TODO: If the shuttle is ready to launch, change the text of launchStatus to green and display “Shuttle is ready for launch”. 
-
- //NOTE: make sure to call formSubmission() at the appropriate time in your script.js file
-
- //-----------------------------------------------------------------------------------------------------------------------------------------------
  
-//  function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel)  <---- this is what the parameter names were set to. I changed to match the variables I personally used. 
 
- function formSubmission(document, faultyItems, pilotName, copilotName, fuelLevel, cargoMass) {
+
+ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     
     console.log("is anything happening");
-    validateInput();
+    validateInput(testInput);
 
     // TODO: update shuttle requirements
     faultyItems.style.visibility = "visible";
-    //pilotStatus or maybe just make faultyItem visible on the cargo status line since the form will be completed
-    
+
+    //pilotStatus    
     pilotStatus.innerHTML = `Pilot ${pilotName} is ready for launch`;
 
     //copilotStatus
     copilotStatus.innerHTML = `Co-pilot ${copilotName} is ready for launch` ;
 
-    //fuelStatus --- need to make if statement for fuel levels
-    
+    //fuelStatus --- need to make if statement for fuel levels    
     if (fuelLevel < 10000) {
         launchStatus.innerHTML = "Shuttle Not Ready for Launch";
         launchStatus.style.color = "red";
         fuelStatus.innerHTML = "Fuel level too low for launch";
-    } else {
-        launchStatus.innerHTML = "Shuttle is Ready for Launch"
-        launchStatus.style.color = "green";
-        fuelStatus.innerHTML ="Fuel level high enough for launch"
+    }
+
+    if (fuelLevel > 10000) {
+        fuelStatus.innerHTML ="Fuel level high enough for launch";
     }
 
 
-    //cargoStatus -- need to make if statement for cargo mass
-    
+    //cargoStatus -- need to make if statement for cargo mass    
     if (cargoMass > 10000) {
         launchStatus.innerHTML = "Shuttle Not Ready for Launch";
         launchStatus.style.color = "red";
         cargoStatus.innerHTML = "Cargo mass too heavy for launch";
-    } else {
-        launchStatus.innerHTML = "Shuttle is Ready for Launch";
-        launchStatus.style.color = "green";
+    } 
+
+    if (cargoMass < 10000) {
         cargoStatus.innerHTML = "Cargo mass low enough for launch";
     }
- };
 
- formSubmit.addEventListener("click", function(event){
-    event.preventDefault();
-    formSubmission();
-});
+    if (cargoMass < 10000 && fuelLevel > 10000){
+        launchStatus.innerHTML = "Shuttle is Ready for Launch"
+        launchStatus.style.color = "green";
+    }
+ };
 
 
  
@@ -162,9 +133,14 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
  
  function pickPlanet(planets) {
  }
+
  
  module.exports.addDestinationInfo = addDestinationInfo;
  module.exports.validateInput = validateInput;
  module.exports.formSubmission = formSubmission;
  module.exports.pickPlanet = pickPlanet; 
  module.exports.myFetch = myFetch;
+
+
+
+ 
