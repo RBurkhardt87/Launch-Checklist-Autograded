7bda909c8795930e48d22fbedd9fb2bc5750e65a
Rachel Burkhardt <r.burkhardt.ibclc@gamil.com>
2024-08-15 21:23:02 -0500

    Still only passing 4/10 test and working on my LaunchChecklist tests

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Write your JavaScript code here!
+
+
 
 // Write your JavaScript code here!
 
@@ -26,13 +27,18 @@ window.addEventListener("load", function() {
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
+    let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
+        console.log(listedPlanetsResponse);
         console.log(listedPlanets);
     }).then(function () {
         console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+       console.log(pickPlanet(listedPlanetsResponse));
+       console.log(pickPlanet(listedPlanets));
+       
+        
     })
     
  });
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,7 +17,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
     */
 
     //or will it be planetsReturned??? Do I need to call pickPlanet()?
-    let planet = data[randomIndex];
+    
 
     const missionTarget = document.getElementById("missionTarget");
     missionTarget.innerHTML = `
@@ -31,7 +31,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                  </ol>
                  <img src="${imageUrl}">`
 
-    return planet;
+  
  }
 
 
@@ -65,89 +65,79 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     const faultyItemsList = document.getElementById("faultyItems");
 
 
-    const pilotInput = document.getElementById("pilotName");
-    const copilotInput = document.getElementsByName("copilotName")[0];
-    const fuelInput = document.getElementsByName("fuelLevel")[0];
-    const cargoInput = document.getElementsByName("cargoMass")[0];
-
-
     //NOTE: this was coming back correctly. This is most likely how I am suppose to call the function and pass in the inputs as arguments. Then I take those validated returns to write my if statements for formSubmission <---maybe?
-
-
-
     const validPilotName = validateInput(pilot);
     const validCopilotName = validateInput(copilot);
     const validFuelInput = validateInput(fuelLevel);
     const validCargoInput = validateInput(cargoLevel);
-   
+               
+ 
 
 
-    if (pilotInput.value.trim() === "" || copilotInput.value.trim() === "" || fuelInput.value.trim() === "" || cargoInput.value.trim() === "") {
-        window.alert("All field are required!");            
-    }
-
-
-    if (validPilotName === "Not a Number" && validCopilotName === "Not a Number" && validFuelInput === "Is a Number" && validCargoInput === "Is a Number"){
-        if (Number(fuelInput.value) >= 10000 && Number(fuelInput.value) <= 10000 ){
-            launchStatus.innerHTML = "Shuttle is Ready for Launch";
-            launchStatus.style.color = "green";
-        }
-    }
-
-
-        if (validPilotName === "Not a Number" ){
-            pilotStatus.innerHTML = `Pilot ${pilotInput.value} is ready for launch`;
+        if (validPilotName === "Empty") {
+            window.alert("All field are required!");
+        } else if (validPilotName === "Not a Number" ){
+            pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
         } else {
-            alert("Pilot name must contain letters only")
             pilotStatus.innerHTML = "Awaiting a Valid Pilot Name";
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-            launchStatus.style.color = "red"; 
+            launchStatus.style.color = "red";   
         }
 
 
-        if (validCopilotName === "Not a Number" ){
-            copilotStatus.innerHTML = `Co-pilot ${copilotInput.value} is ready for launch`;
+        if (validCopilotName === "Empty"){  
+            window.alert("All field are required!"); 
+        }else if (validCopilotName === "Not a Number" ){
+            copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
         } else {
-            alert("Co-pilot name must contain letters only")
             copilotStatus.innerHTML = "Awaiting a Valid Co-pilot Name";
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
             launchStatus.style.color = "red"; 
         }
 
 
-        if (validFuelInput === "Empty" || validFuelInput === "Not a Number"){
-            alert("Fuel level must be a number of 10,000 or greater to launch");
+        if (validFuelInput=== "Empty"){
+            window.alert("All field are required!");
+        } else if (validFuelInput === "Not a Number"){
             fuelStatus.innerHTML = "Awaiting a Proper Fuel Level to Launch";
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
             launchStatus.style.color = "red";
-        } else {
-            if (Number(fuelInput.value) < 10000) {
+        } else if (validFuelInput === "Is a Number" ){
+            if (Number(fuelLevel) < 10000) {
                 launchStatus.innerHTML = "Shuttle Not Ready for Launch";
                 launchStatus.style.color = "red";
                 fuelStatus.innerHTML = "Fuel level too low for launch";
-            } else if (Number(fuelInput.value) >= 10000) {
+            } else if (Number(fuelLevel) >= 10000) {
                 fuelStatus.innerHTML ="Fuel level high enough for launch";
             
             }
         }
 
 
-        if (validCargoInput === "Empty" || validCargoInput === "Not a Number"){
-            alert("Cargo mass must be a number of 10,000 or less to launch");
+          
+        if (validCargoInput === "Empty") {
+            window.alert("All field are required!");
+        } else if (validCargoInput === "Not a Number"){
             cargoStatus.innerHTML = "Awaiting a Proper Cargo Mass Level to Launch";
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
             launchStatus.style.color = "red";
-        } else {
-            if (Number(cargoInput.value) > 10000) {
+        } else if (validCargoInput === "Is a Number" && validCargoInput !== "Empty"){
+            if (Number(cargoLevel) > 10000) {
                 launchStatus.innerHTML = "Shuttle Not Ready for Launch";
                 launchStatus.style.color = "red";
                 cargoStatus.innerHTML = "Cargo mass too heavy for launch";
-            } else if (Number(fuelInput.value) <= 10000) {
+            } else if (Number(cargoLevel) <= 10000) {
                 cargoStatus.innerHTML ="Cargo mass low enough for launch";
             }
         }
-        
-        faultyItemsList.style.visibility = "visible";
+
+        if (validPilotName === "Not a Number" && validCopilotName === "Not a Number" && validFuelInput === "Is a Number" && validCargoInput === "Is a Number"){
+            if (Number(fuelLevel) >= 10000 && Number(cargoLevel) <= 10000 ){
+                launchStatus.innerHTML = "Shuttle is Ready for Launch";
+                launchStatus.style.color = "green";
+            }
+        }
+        faultyItemsList.style.visibility= "visible";
 
 };
 
