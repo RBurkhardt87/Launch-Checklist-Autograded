50f9a1f2a762473b69423bde9db6188fb7990490
Rachel Burkhardt <r.burkhardt.ibclc@gamil.com>
2024-08-16 23:52:30 -0500

    All test are complete and I believe everything is displaying correctly in browser

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
-
-
-
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
-
-   
-
-    myFetch();
+ 
+  
     const form = document.querySelector('form');
     form.addEventListener("submit", function(event) {
-        
-        event.preventDefault(); 
-
+    event.preventDefault(); 
         
         const pilotInput = document.getElementById("pilotName").value;
         const copilotInput = document.getElementsByName("copilotName")[0].value;
         const fuelInput = document.getElementsByName("fuelLevel")[0].value;
         const cargoInput = document.getElementsByName("cargoMass")[0].value;
         const faultyItemsList = document.getElementById("faultyItems");
-
         
         formSubmission(document, faultyItemsList, pilotInput, copilotInput, fuelInput, cargoInput);
     });
-
+ 
 
     let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse= myFetch();
     listedPlanetsResponse.then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanetsResponse);
-        console.log(listedPlanets);
+    listedPlanets =result;
+    console.log(listedPlanets);
     }).then(function () {
-        console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-       console.log(pickPlanet(listedPlanetsResponse));
-       console.log(pickPlanet(listedPlanets));
-       
-        
-    })
+         
+
+    planet = pickPlanet(listedPlanets);
     
- });
+
+    const planetName = planet.name;
+    const planetDiameter = planet.diameter;
+    const planetStar = planet.star;
+    const planetDistance = planet.distance;
+    const planetMoons = planet.moons;
+    const planetImage = planet.image;
+  
+    
+    addDestinationInfo(document, planetName, planetDiameter, planetStar, planetDistance, planetMoons, planetImage);
+});
+});
+
+
+
+
+
--- scriptHelper.js
+++ scriptHelper.js
@@ -3,36 +3,20 @@
 require('cross-fetch/polyfill');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-    // Here is the HTML formatting for our mission target div.
-    /*
-                 <h2>Mission Destination</h2>
-                 <ol>
-                     <li>Name: </li>
-                     <li>Diameter: </li>
-                     <li>Star: ${star}</li>
-                     <li>Distance from Earth: </li>
-                     <li>Number of Moons: </li>
-                 </ol>
-                 <img src="">
-    */
-
-    //or will it be planetsReturned??? Do I need to call pickPlanet()?
-    
+  
 
     const missionTarget = document.getElementById("missionTarget");
     missionTarget.innerHTML = `
-           <h2>Mission Destination</h2>
-                 <ol>
-                     <li>Name: ${name} </li>
-                     <li>Diameter: ${diameter} </li>
-                     <li>Star: ${star}</li>
-                     <li>Distance from Earth: ${distance} </li>
-                     <li>Number of Moons: ${moons} </li>
-                 </ol>
-                 <img src="${imageUrl}">`
-
-  
- }
+            <h2>Mission Destination</h2>
+                  <ol>
+                      <li>Name: ${name} </li>
+                      <li>Diameter: ${diameter} </li>
+                      <li>Star: ${star}</li>
+                      <li>Distance from Earth: ${distance} </li>
+                      <li>Number of Moons: ${moons} </li>
+                  </ol>
+                  <img src="${imageUrl}">`
+};
 
 
 
@@ -56,7 +40,7 @@ function validateInput(testInput) {
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
-    //NOTE: variables for the formSubmission/update LaunchStatus
+
     const launchStatus = document.getElementById("launchStatus");
     const pilotStatus = document.getElementById("pilotStatus");
     const copilotStatus = document.getElementById("copilotStatus");
@@ -65,72 +49,63 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     const faultyItemsList = document.getElementById("faultyItems");
 
 
-    //NOTE: this was coming back correctly. This is most likely how I am suppose to call the function and pass in the inputs as arguments. Then I take those validated returns to write my if statements for formSubmission <---maybe?
     const validPilotName = validateInput(pilot);
     const validCopilotName = validateInput(copilot);
     const validFuelInput = validateInput(fuelLevel);
-    const validCargoInput = validateInput(cargoLevel);
-               
- 
+    const validCargoInput = validateInput(cargoLevel);               
     
+
     if (validPilotName === "Empty" || validCopilotName === "Empty" || validFuelInput=== "Empty" || validCargoInput === "Empty" ) {
         alert("All field are required!");
-    }
-    if (validPilotName === "Is a Number" || validCopilotName === "Is a Number" || validFuelInput=== "Not a Number" || validCargoInput === "Not a Number" ){
+        launchStatus.innerHTML="Awaiting Information Before Launch";
+    } else if (validPilotName === "Is a Number" || validCopilotName === "Is a Number" || validFuelInput=== "Not a Number" || validCargoInput === "Not a Number" ){
         alert("Pilot and copilot must be valid names. Fuel level and cargo mass must be valid numbers");
     } else {  
         faultyItemsList.style.visibility= "visible";
         pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
         copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
     }
-    if (Number(fuelLevel) < 10000) {
+
+    if (validFuelInput !== "Empty" && Number(fuelLevel) < 10000) {
         launchStatus.innerHTML = "Shuttle Not Ready for Launch";
         launchStatus.style.color = "red";
         fuelStatus.innerHTML = "Fuel level too low for launch";
     } else if (Number(fuelLevel) >= 10000) {
         fuelStatus.innerHTML ="Fuel level high enough for launch";        
     }
-    if (Number(cargoLevel) > 10000) {
-        launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-        launchStatus.style.color = "red";
+
+    if (validCargoInput !== "Empty" && Number(cargoLevel) > 10000) {
         cargoStatus.innerHTML = "Cargo mass too heavy for launch";
     } else if (Number(cargoLevel) <= 10000) {
         cargoStatus.innerHTML ="Cargo mass low enough for launch";
-    }    
-    if (Number(fuelLevel) >= 10000 && Number(cargoLevel) <= 10000 ){
+    }  
+
+    if(Number(fuelLevel) >= 10000 && Number(cargoLevel) <= 10000 ){
             launchStatus.innerHTML = "Shuttle is Ready for Launch";
             launchStatus.style.color = "green";
         }
+    };
     
-};
-
 
 async function myFetch() {
     let planetsReturned;
- 
-    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
-        response.json().then( function (data){
-        // console.log(data);
+    let data;
 
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response){
+        data = response.json()
+    })
 
-        // allPlanets = [];
-        // allPlanets.push(data);
-        // console.log(allPlanets);
-
-
-    });
-    });  
-    
-     return planetsReturned;
-
+    return data;    
 }
-
- //NOTE: I think that's how we did it in Prep Example 1 with an array--- may need to be adjusted to work with a fetched json file.
+   
+    
 function pickPlanet(planets) {
-    let randomIndex = Math.floor(Math.random()* planets.length);
-    // console.log(planets[randomIndex]);
+    let randomIndex = Math.floor(Math.random()* planets.length);   
+    
     return planets[randomIndex];
 }
+
+
 
 
 
